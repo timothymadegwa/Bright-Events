195996989fb4e56507b1ca6bbde989453cbe408e
Timothy Madegwa <timothymadegwa@gmail.com>
2017-11-22 11:39:46 +0300

    this was the branch

---
+++ UI/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StingField, PasswoedField
+from wtforms.validators import InputRequired, email, length
+app= Flask(__name__)
+app.config['SECERET_KEY']='THISISTHESECRETKEYFORTHEAPP'
+Bootstrap(app)
